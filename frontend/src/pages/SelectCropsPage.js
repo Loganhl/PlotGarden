9afbc524632aa92098e28ce8c0ca667f1116cb8e
amandanngo <amandanngo@gmail.com>
2024-11-02 15:58:41 -0500

    Conditional Symbol Rendering in SelectCropsPage
    -conditionally render symbols depending on if plant id is in according array
    -currently implemented using test arrays until route is fully developed

--- frontend/src/pages/SelectCropsPage.js
+++ frontend/src/pages/SelectCropsPage.js
@@ -12,6 +12,19 @@ import circle3 from '../assets/circle-3.png'
 function SelectCropsPage(){
     const [plants, setPlants] = useState([]);
 
+    //Star
+    let matching_plants = [410, 663, 665, 667, 671, 682, 685, 688, 689, 691, 693, 694, 697, 862, 958, 978, 984, 1026, 1144, 1273, 1316, 1320, 1321, 1324, 1325, 1327, 1328, 1332, 1333, 1605, 1836, 1885, 2098, 2251, 2252, 2255, 2795, 3013, 3062, 8065];
+
+    //Square
+    let recommended_crops = [693, 862, 978, 984, 1144, 1596, 1598, 1599, 1602, 1836, 2098, 2280, 2795, 3234, 3387, 5535, 6539, 6963, 7109, 7405];
+
+    //Maintenance Level
+    let maintain_categories = {
+        'Low': [682, 685, 688, 689, 691, 693, 694, 731, 958, 1026, 1273, 1316, 1320, 1321, 1324, 1325, 1327, 1328, 1595, 1598, 1602, 1605, 1836, 2098, 2283, 3387, 4302, 5498, 5533, 5535, 6236, 7833, 7843, 8489], 
+        'Moderate': [410, 663, 665, 667, 671, 697, 862, 978, 984, 1144, 1332, 1333, 1596, 1599, 1600, 1885, 2251, 2252, 2280, 2795, 3234, 4463, 4626, 5021, 5022, 5023, 5024, 5841, 6169, 6539, 6766, 6950, 6952, 6963, 7109, 7405, 7409, 8039, 8062, 8065, 8658], 
+        'High': [791, 2255, 3013, 3024, 3025, 8759]
+    };
+
     useEffect(() => {
         axios.get('http://127.0.0.1:5000/api/plants')
             .then(response => {
@@ -22,6 +35,8 @@ function SelectCropsPage(){
         });
     }, []);
 
+    console.log(plants);
+    
 
     return(
         <div>
@@ -34,12 +49,30 @@ function SelectCropsPage(){
                             <input type="checkbox"></input>
                             <Link to={`/crop/${plant.id}`}>{plant.common_name}</Link>
                         </label>
-                        <img src={square} height={10} width={10} />
-                        <img src={star} height={10} width={10} />
-                        <img src={circle1} height={10} width={10} />
+                        
+                        {matching_plants.includes(parseInt(`${plant.id}`)) && 
+                            <img src={star} height={10} width={10} />
+                        }
+                        
+                        {recommended_crops.includes(parseInt(`${plant.id}`)) && 
+                            <img src={square} height={10} width={10} />
+                        }
+
+                        {maintain_categories['Low'].includes(parseInt(`${plant.id}`)) && 
+                            <img src={circle1} height={10} width={10} />
+                        }
+
+                        {maintain_categories['Moderate'].includes(parseInt(`${plant.id}`)) && 
+                            <img src={circle2} height={10} width={10} />
+                        }
+
+                        {maintain_categories['High'].includes(parseInt(`${plant.id}`)) && 
+                            <img src={circle3} height={10} width={10} />
+                        }
 
                     </li>
-                ))}
+                ))
+                }
             </ul>
 
             <div>
@@ -51,9 +84,13 @@ function SelectCropsPage(){
                 </div>
                 <div>
                      <img src={star} height={10} width={10} />
+                    <inline>Matches Region</inline>
                 </div>
                 <div>
                      <img src={circle1} height={10} width={10} />
+                    <inline>Low Maintenance Level</inline>
+                     
+
                 </div>
             </div>
         </div>
