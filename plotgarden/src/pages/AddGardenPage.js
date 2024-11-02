4f5693ca54922fa70b0ded7f0844ccc081b4f2ef
amandanngo <amandanngo@gmail.com>
2024-11-01 22:54:31 -0500

    Populated AddGardenPage
    -Added all the html components for the form to add a garden
    -Added state changes
    -Waiting on backend progress to handle submit

--- plotgarden/src/pages/AddGardenPage.js
+++ plotgarden/src/pages/AddGardenPage.js
@@ -1,9 +1,62 @@
+import { useState, useContext } from 'react';
 
 
 function AddGardenPage(){
+
+    const [state, setState] = useState({
+        gardenLength: '',
+        gardenWidth: '',
+        region: ''
+    });
+
+    const updateState = event => setState({
+        ...state,
+        [event.target.name]: event.target.value
+    });
+
+    const handleSubmit = event => {
+        event.preventDefault();
+       //Post to Backend
+    }
+
     return(
         <div>
             <h1>Add Garden Page</h1>
+
+            <form onSubmit={handleSubmit}>
+    
+                <div>
+                    <label for="garden-size">Garden Size</label>
+                    <input 
+                        type="text"
+                        name="gardenLength"
+                        value={state.gardenLength}
+                        onChange={updateState}
+                    />
+                    <inline>ft</inline>
+                    <input 
+                        type="text"
+                        name="gardenWidth"
+                        value={state.gardenWidth}
+                        onChange={updateState}
+                    />
+                    <inline>ft</inline>
+                </div>
+
+                <div>
+                    <label for="region">Region</label>
+                    <input 
+                        type="text"
+                        name="region"
+                        value={state.region}
+                        onChange={updateState}
+                    />
+
+                </div>
+
+                <button>Submit</button>
+
+            </form>
         </div>
     )
 }
