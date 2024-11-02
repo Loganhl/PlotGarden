007553fd0bc6c9851370a92cb4da8e07bbc4b2c7
crewskaelyn <crewskaelyn@gmail.com>
2024-11-02 05:27:01 -0500

    connecting backend to frontend and making calls to database

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,25 @@
 import sqlite3
-from flask import Flask, render_template, request, url_for, flash, redirect, abort
+from flask import Flask, jsonify
+from flask_cors import CORS
+import os
 
 
 app = Flask(__name__)
+CORS(app)
 app.config["DEBUG"] = True
 app.config['SECRET_KEY'] = 'secret key'
 
 def get_db_connection():
-    conn = sqlite3.connect('/database/')
-
+    db_path = os.path.join(os.path.dirname(__file__), '..', 'database', 'database.db')
+    conn = sqlite3.connect(db_path)
     conn.row_factory = sqlite3.Row
-
     return conn
 
-@app.route('/')
-def index():
+@app.route('/api/plants', methods=['GET'])
+def get_plants():
+    conn = get_db_connection()
+    plants = conn.execute('SELECT * FROM plants').fetchall()
+    conn.close()
+
+    return jsonify([dict(plant) for plant in plants])
     
-    return render_template('index.html')
--- frontend/src/pages/SelectCropsPage.js
+++ frontend/src/pages/SelectCropsPage.js
@@ -1,9 +1,29 @@
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function SelectCropsPage(){
+    const [plants, setPlants] = useState([]);
+
+    useEffect(() => {
+        console.log("useEffect is running");
+        axios.get('http://127.0.0.1:5000/api/plants')
+            .then(response => {
+                console.log(response.data);
+                setPlants(response.data);
+        })
+        .catch(error => {
+            console.error("There was an error fetching the garden data!", error)
+        });
+    }, []);
+
     return(
         <div>
-            <h1>Select Crops Page</h1>
+            <h1>Select Crops</h1>
+            <ul>
+                {plants.map(plant => (
+                    <li key={plant.id}>{plant.common_name}</li>
+                ))}
+            </ul>
         </div>
     )
 }
