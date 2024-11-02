d30a73493095471387671809107165899bf7369d
amandanngo <amandanngo@gmail.com>
2024-11-02 18:00:52 -0500

    Fix present-plants route in app.py

--- frontend/src/pages/GuidePage.js
+++ frontend/src/pages/GuidePage.js
@@ -26,8 +26,16 @@ function GuidePage(){
             <div>
                 <h2>Selecting Your Crops!</h2>
                 <p>Choosing the right crops is a crucial step in creating a successful garden. Here are some important factors to consider:</p>
-            
-            
+                <ul>
+                    <li><b>Nutrient Density:</b> Aim to select crops that provide high nutritional value. Nutrient-dense vegetables, such as kale, spinach, and broccoli, offer essential vitamins and minerals that contribute to a healthy diet. Our website provides a list of recommended crops that are not only tasty but also packed with nutrients, helping you make the most of your garden space.</li>
+                    <li><b>Hardiness Zone:</b> Understanding your hardiness zone is essential for selecting crops that will thrive in your climate. The hardiness zone indicates the average minimum winter temperature in your area, guiding you in choosing plants that can survive local conditions. Our site will assess your garden's location to determine its hardiness zone and suggest crops that are well-suited to that environment.</li>
+                    <li><b>Size of Crops:</b> Consider the size of the crops you want to grow. Some plants require more space than others; for instance, tomatoes and peppers need more room to spread out, while lettuce and radishes can fit more closely together. When you select crops through our form, our website will generate a detailed plot of your garden, including recommended locations and size measurements for each crop. This ensures efficient use of space and helps you visualize how everything will fit together.</li>
+                </ul>
+                <p>By considering nutrient density, hardiness zone, and crop size, you'll be well on your way to selecting a variety of plants that not only flourish in your garden but also provide you with a bountiful harvest. For more guidance, visit Texas A&M AgriLife Extension's Easy Gardening Series (https://aggie-horticulture.tamu.edu/vegetable/easy-gardening-series/) for additional resources and tips.</p>
+            </div>
+            <div>
+                <h2>How to Maintain Your Garden!</h2>
+                <p>Maintaining your garden is essential for ensuring healthy plants and a fruitful harvest. Here are the key aspects of garden maintenance to keep in mind:</p>
             </div>
         </div>
     )
