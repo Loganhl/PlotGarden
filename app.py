85adde28c46bec48f40ad6cb86eeecac3aff9f3f
crewskaelyn <crewskaelyn@gmail.com>
2024-11-01 21:25:06 -0500

    starting sqlite database

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
+import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect, abort
 
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 app.config['SECRET_KEY'] = 'secret key'
+
+def get_db_connection():
+    conn = sqlite3
+
+    conn.row_factory = sqlite3.Row
+
+    return conn
 
 @app.route('/')
 def index():
---
+++ database/schema.sql
@@ -0,0 +1,17 @@
+CREATE TABLE gardens (
+    garden_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    garden_name TEXT NOT NULL,
+    description TEXT,
+    location TEXT,
+    plot TEXT,
+    image_link TEXT,
+    plot_size INTEGER
+};
+
+CREATE TABLE crops (
+    crop_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    garden_id INTEGER NOT NULL,
+    crop_name TEXT NOT NULL,
+    crop_id INTEGER NOT NULL,
+    FOREIGN KEY (garden_id) REFERENCES gardens(garden_id)
+);
