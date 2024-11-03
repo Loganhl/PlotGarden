08fe6efb8fa4786465847eefc9420eab826ec299
Logan Limbaugh <lhunterlimbaugh@gmail.com>
2024-11-03 09:55:30 -0600

    Revert "Update app.py"
    This reverts commit 4b33b85dddbd2f0f7163047e0b9d8e02231779b0.

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,16 @@
 import sqlite3
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-import os
 from pairing import present_plant_ids
+from dotenv import load_dotenv
+import os
 from create_cluster import create_garden_plot
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
 
-app.config["DEBUG"] = True
-app.config['SECRET_KEY'] = 'secret key'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 def get_db_connection():
     print("Connected to Database")
@@ -21,22 +22,13 @@ def get_db_connection():
 def convert_user_id(user_id):
     return user_id[-9:]
 
-def is_last_crop(garden_id):
-    # Function to check if there are no more crops for the garden
-    conn = get_db_connection()
-    try:
-        count = conn.execute('SELECT COUNT(*) FROM crops WHERE garden_id = ?', (garden_id,)).fetchone()[0]
-        # Return True if it's the last crop added
-        return count >= 1  # Modify this logic based on your needs
-    finally:
-        conn.close()
-
 
 @app.route('/api/plants', methods=['GET'])
 def get_plants():
     conn = get_db_connection()
     plants = conn.execute('SELECT * FROM plants').fetchall()
     conn.close()
+
     return jsonify([dict(plant) for plant in plants])
 
 @app.route('/api/present-plants/<int:garden_id>', methods=["GET"])
@@ -45,24 +37,29 @@ def get_present_plants(garden_id):
     conn = get_db_connection()  # Test
     try:
         plants_data = present_plant_ids(garden_id)
+
         if plants_data:
+            #print(f"Plants data retrieved: {plants_data}") 
             return jsonify(plants_data)
         else:
             print("No plants found for this garden ID.")
             return jsonify({"message": "No plants found for this garden ID."}), 404
+            
     except Exception as e:
         print(f"An error occurred: {e}")  
         return jsonify({"error": str(e)}), 500 #server error response
     finally:
         conn.close()
-
+    
 @app.route('/api/crop/<int:id>', methods=["GET"])
 def get_crop(id):
     conn = get_db_connection()
     crop = conn.execute('SELECT * FROM plants WHERE id = ?', (id,)).fetchone()
     conn.close()
+
     if crop is None:
         return jsonify({"error": "Plant not found"}), 404
+    
     return jsonify(dict(crop))
 
 @app.route('/api/crop', methods=["POST"])
@@ -85,28 +82,27 @@ def add_crops():
         VALUES (?,?,?)
     '''
 
-    params = (garden_id, id, crop_name)
+    params = (garden_id,id,crop_name)
 
     try:
         conn.execute(insert_query, params)
         conn.commit()
-
-        # Check if it's the last crop added and if so, create the garden plot
-        if is_last_crop(garden_id):
-            create_garden_plot(garden_id)
-
+        #create_garden_plot(garden_id)
         return jsonify({"garden_id": garden_id}), 201 
     except Exception as e:
         return jsonify({"error": str(e)}), 400
+
     finally:
         conn.close()
 
 @app.route('/api/gardens/<int:id>', methods=["GET"])
 def get_gardens(id):
+
     conn = get_db_connection()
     gardens = conn.execute('SELECT * FROM gardens WHERE user_id = ?', (id,)).fetchall()
     print(gardens)
     conn.close()
+
     return jsonify([dict(garden) for garden in gardens])
 
 @app.route('/api/garden/<int:id>', methods=["GET"])
@@ -114,8 +110,10 @@ def get_garden(id):
     conn = get_db_connection()
     garden = conn.execute('SELECT * FROM gardens WHERE garden_id = ?', (id,)).fetchone()
     conn.close()
+
     if garden is None:
         return jsonify({"error": "Garden not found"}), 404
+    
     return jsonify(dict(garden))
 
 @app.route('/api/crops/<int:id>', methods=["GET"])
@@ -123,11 +121,14 @@ def get_crops(id):
     conn = get_db_connection()
     crops = conn.execute('SELECT * FROM crops WHERE garden_id = ?', (id, )).fetchall()
     conn.close()
+
     return jsonify([dict(crop) for crop in crops])
+
 
 @app.route('/api/gardens', methods=["POST"])
 def add_garden():
     conn = get_db_connection()
+
     data = request.get_json()
 
     garden_name = data.get('garden_name')
@@ -153,5 +154,6 @@ def add_garden():
         return jsonify({"garden_id": garden_id}), 201 
     except Exception as e:
         return jsonify({"error": str(e)}), 400
+
     finally:
         conn.close()
