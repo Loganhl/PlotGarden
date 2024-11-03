b1daa4a92d62539648bd8139277f3e2dc0e363a5
crewskaelyn <crewskaelyn@gmail.com>
2024-11-03 04:53:28 -0600

    adding about/landing page

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -2,8 +2,27 @@
 
 function LandingPage(){
     return(
-        <div>
-            <h1>Landing Page</h1>
+        <div className="about">
+            <h1>Welcome to PlotGarden!</h1>
+            <div className ="about-content">
+            <p>People all accross the United States struggle with a massive problem, food deserts.
+                This is an area in which a community finds it difficult to buy affordable or good- quality fresh food.
+                Nutrition and our diets contribute to many problems that our nation is facing as they impact nearly every
+                aspect of our physcial and mental lives.
+            </p>
+            <h3>We want to help tackle this problem!</h3>
+            <p>We created a highly accessible and interactive website that guides a user
+                through the steps of creating a garden from start to finish. 
+                We placed our main efforts in collecting data from multiple sourcces 
+                and not only combining it into user-friendly data, but also presenting users with
+                a plotting of their garden based on their entered metrics such as garden location and size.
+            </p>
+            <h3>Getting Started:</h3>
+            <p>Through the navigation menu, you can access our extensive plant list and find care information,
+                or visit the guide page for how to get your garden started!
+                <br /><br />Log in to make your garden now!
+            </p>
+            </div>
         </div>
     )
 }
